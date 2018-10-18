a59e728c88875df819bd94c8005fbc4d0c74fd18
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-10-18 13:57:50 +0200

    continuation

--- BMIcalculator.py
+++ BMIcalculator.py
@@ -32,25 +32,57 @@ answer = YorN()
 if answer == 'yes' or answer == 'y': #if 'yes'.startswith(answer): <= another option
     while True:
         try:
-            weight = int(input('What\'s your weight? '))
-            break
+            weight = float(input('What\'s your weight in range 0 - 250kg? >>> '))
         except ValueError:
             print('Please enter a number.')
-        try:
-            
+        else:
+            if 0.0 < weight < 251.0:
+                break
+            else:
+                print('It\'s not in range!')
 elif answer.startswith('n'):
     print ('Have a nice day!')
     time.sleep(2)
     exit()
 
+while True:
+    try:
+        height = float(input('What\'s your height in centimeters? >>>'))
+        break
+    except ValueError:
+        print ('Use numbers only!')
+height = height / 100
+print ('Ok, I\'ve got all data needed.')
+time.sleep(1)
+print ('Now to count your BMI (Body mass index) I will divide your weight which is:', str(weight) + 'kg by your height:', str(height) + 'm.')
+print()
+wh = round((weight / height),2)
+time.sleep(2)
+print ('It gives us', str(wh) +'.')
+print()
+time.sleep(1)
+print ('The result I will divide again by your height.')
+time.sleep(1)
+print ('So, we have your BMI!')
+print()
+bmi = wh / height
+bmi = round(bmi,2)
+time.sleep(1)
+print ('It is', str(bmi)+ '!!!')
 
-
-print('ok')
-
-
-
-
-
+time.sleep(2)
+if 18.5 >= bmi:
+    print ('It means that you are underweight. Try to eat a bit more and get some weight ;)')
+elif 18.5 < bmi <= 24.9:
+    print ('It means that you are normal. Keep it up!')
+elif 24.9 < bmi <= 29.9:
+    print ('It means that you are overweight! That\'s not good. Try to eat less, do some exercises.')
+    time.sleep(1)
+    print ('It\'s about your health and life!')
+elif 30 <= bmi:
+    print ('Your situation is serious. You are really obese!')
+    time.sleep(1)
+    print('You have to lose weight man!!! Really, think about it.')
 
 
 
